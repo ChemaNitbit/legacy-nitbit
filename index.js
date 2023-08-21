7c928e32f42dfd3476c5ed05df8e1ae7d3c0f442
Jose Maria CLi <chema9703@gmail.com>
2023-08-20 23:52:56 -0600

    chore: locate the firebase inicialization on the root file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import {PaperProvider} from 'react-native-paper';
+import './src/shared/infrastructure/firebase-config';
 
 export default function Main() {
   return (
