9c2a7a805bf11ad496099d0f394b76d9913be8ae
Jose Maria CLi <chema9703@gmail.com>
2023-08-21 00:58:10 -0600

    feat: first draft for the custom bottom tab bar

--- src/components/layout/NbTabBar/index.tsx
+++ src/components/layout/NbTabBar/index.tsx
@@ -1,10 +1,88 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export const NbTabBar = () => {
+const TabIcon = ({selected}: any) => {
   return (
-    <View>
-      <Text>CustomTabBar</Text>
+    <View style={[styles.tabIcon, selected && styles.selectedTab]}>
+      <MaterialCommunityIcons
+        name="message-text-outline"
+        color={'black'}
+        size={30}
+      />
     </View>
   );
 };
+
+export const NbTabBar = ({state, descriptors, navigation}) => {
+  return (
+    <View style={styles.tabBar}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const isFocused = state.index === index;
+
+        let icon;
+
+        if (index === 2) {
+          // Special handling for the 3rd option (user avatar)
+          icon = <View style={styles.avatarContainer} />;
+        } else {
+          // Other icons
+          icon = (
+            <MaterialCommunityIcons
+              name="message-text-outline"
+              color={'black'}
+              size={30}
+            />
+          ); // Replace with your actual icon component
+        }
+
+        return (
+          <TouchableOpacity
+            key={index}
+            onPress={() => navigation.navigate(route.name)}
+            style={styles.tabButton}>
+            <TabIcon icon={icon} selected={isFocused} />
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white', // Adjust as needed
+  },
+  tabButton: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  tabIcon: {
+    width: 24,
+    height: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  selectedTab: {
+    backgroundColor: 'green', // Adjust as needed
+    borderRadius: 20, // Half of the icon height
+  },
+  avatarContainer: {
+    width: 28,
+    height: 28,
+    borderColor: 'green', // Adjust as needed
+    borderWidth: 2,
+    borderRadius: 14, // Half of the avatar size
+    overflow: 'hidden',
+  },
+  avatarImage: {
+    width: 24,
+    height: 24,
+    resizeMode: 'cover',
+  },
+});
