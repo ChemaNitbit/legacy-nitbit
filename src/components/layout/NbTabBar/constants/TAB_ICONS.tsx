7a2f3571f38bdc07f1bfb4ba7c1e6bacf4a7f7e1
Jose Maria CLi <chema9703@gmail.com>
2023-08-27 18:00:07 -0600

    feat: use different iconst for each tab bar menu item

--- src/components/layout/NbTabBar/constants/TAB_ICONS.tsx
+++ src/components/layout/NbTabBar/constants/TAB_ICONS.tsx
@@ -4,37 +4,19 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 export const TAB_ICONS: {[key in string]: ReactElement} = {
   OthersTab: (
     <MaterialCommunityIcons
-      name="message-text-outline"
-      size={30}
+      name="plus-circle-outline"
+      size={25}
       color="black"
     />
   ),
   MessagesTab: (
-    <MaterialCommunityIcons
-      name="message-text-outline"
-      size={30}
-      color="black"
-    />
+    <MaterialCommunityIcons name="email-outline" size={25} color="black" />
   ),
   ProfileTab: (
-    <MaterialCommunityIcons
-      name="message-text-outline"
-      size={30}
-      color="black"
-    />
+    <MaterialCommunityIcons name="account-circle" size={25} color="black" />
   ),
-  GeoFencesTab: (
-    <MaterialCommunityIcons
-      name="message-text-outline"
-      size={30}
-      color="black"
-    />
-  ),
+  GeoFencesTab: <MaterialCommunityIcons name="web" size={25} color="black" />,
   HomeTab: (
-    <MaterialCommunityIcons
-      name="message-text-outline"
-      size={30}
-      color="black"
-    />
+    <MaterialCommunityIcons name="home-outline" size={25} color="black" />
   ),
 };
