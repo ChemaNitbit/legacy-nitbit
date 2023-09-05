fd2870eb6c0f23548e96a2a62a75ae21ac2bb8af
Jose Maria CLi <chema9703@gmail.com>
2023-09-04 20:17:27 -0600

    feat: prepare initial tab bar

--- src/components/layout/NbTabBar/constants/styles.ts
+++ src/components/layout/NbTabBar/constants/styles.ts
@@ -1,31 +1,36 @@
 import {StyleSheet} from 'react-native';
 
+export const ICON_CONTAINER_SIZE = 45;
+export const AVATAR_SIZE = 40;
+export const ICON_SIZE = 24;
+
 export const styles = StyleSheet.create({
   tabBar: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: 'white', // Adjust as needed
+    backgroundColor: '#FFFFFF', // Adjust as needed
+    padding: 0,
   },
   tabButton: {
     flex: 1,
     alignItems: 'center',
-    paddingVertical: 10,
+    paddingVertical: 5,
   },
   tabIcon: {
-    width: 24,
-    height: 24,
+    width: ICON_CONTAINER_SIZE,
+    height: ICON_CONTAINER_SIZE,
     alignItems: 'center',
     justifyContent: 'center',
   },
   selectedTab: {
-    backgroundColor: 'green', // Adjust as needed
-    borderRadius: 20, // Half of the icon height
+    backgroundColor: '#4776E6', // Adjust as needed
+    borderRadius: ICON_CONTAINER_SIZE / 2, // Half of the icon height
   },
   avatarContainer: {
     width: 28,
     height: 28,
-    borderColor: 'green', // Adjust as needed
+    borderColor: '#4776E6', // Adjust as needed
     borderWidth: 2,
     borderRadius: 14, // Half of the avatar size
     overflow: 'hidden',
