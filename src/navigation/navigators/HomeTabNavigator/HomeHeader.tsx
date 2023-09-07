b8f77333078a9bb20075420bc780b73b60ef9490
Jose Maria CLi <chema9703@gmail.com>
2023-09-07 00:37:30 -0600

    feat: add a header prepared for the HomeScreen

---
+++ src/navigation/navigators/HomeTabNavigator/HomeHeader.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {NativeStackHeaderProps} from '@react-navigation/native-stack';
+import {Layout} from '../../../components/layout';
+import NBButton from '../../../components/nb-button/NBButton';
+import {Text} from 'react-native-paper';
+
+/**
+ * Customize header for HomeTabNavigator
+ */
+export const HomeHeader = (props: NativeStackHeaderProps): JSX.Element => {
+  return (
+    <Layout.NbHeader
+      leftAction="logo"
+      allowGoingBack={false}
+      rightActions={[
+        {
+          key: 'SEARCH_BUTTON',
+          element: <NBButton onPress={() => null} iconName="magnify" />,
+        },
+        {
+          key: 'NOTIFICATIONS_BUTTON',
+          element: (
+            <NBButton
+              onPress={() => props.navigation.navigate('NotificationsScreen')}
+              iconName="bell-outline"
+            />
+          ),
+        },
+      ]}
+      {...props}>
+      <Text>Friends' Stories</Text>
+    </Layout.NbHeader>
+  );
+};
