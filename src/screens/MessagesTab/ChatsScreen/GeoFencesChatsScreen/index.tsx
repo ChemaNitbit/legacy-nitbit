a1a87323c822f1ee7b11011323565e6f72a8ff1f
José María CL <chema9703@gmail.com>
2023-09-29 18:56:03 -0600

    feat: prepare the geofences chats screen based on the friends screen

--- src/screens/MessagesTab/ChatsScreen/GeoFencesChatsScreen/index.tsx
+++ src/screens/MessagesTab/ChatsScreen/GeoFencesChatsScreen/index.tsx
@@ -1,8 +1,12 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {PaperProvider, Text} from 'react-native-paper';
 import {MessagesRootStackParamList} from '../../../../navigation/navigators/MessagesTabNavigator/MessagesRootStackParamList';
 import {ChatsList} from '../ChatList';
+import {Animated, View, ScrollView, StyleSheet} from 'react-native';
+import {Layout} from '../../../../components/layout';
+import {useListHiddingHeader} from '../../../../hooks/animations/useListHiddingHeader';
+import {ContactsShortcut} from '../ConnectionsChatsScreen/ContactsShortcut';
+import {NbSearchBar} from '../ConnectionsChatsScreen/SearchBar';
 
 export const GeoFencesChatsScreen = (
   props: NativeStackScreenProps<
@@ -10,10 +14,96 @@ export const GeoFencesChatsScreen = (
     'GeoFencesChatsScreen'
   >,
 ): JSX.Element => {
+  const {isHeaderHidden, translateYAnim, onScroll} = useListHiddingHeader({
+    initialThreshold: 5,
+    topTranslation: -80,
+  });
+
+  const fakeChats = (length: number) => {
+    return Array.from({length}, () => ({
+      id: Math.random().toString(),
+      user: {
+        id: Math.random().toString(),
+        name: 'Marjoury',
+      },
+      lastMessage: {
+        plainMessage: 'Hola! Cómo estás?',
+        rawMessage: 'Hola! Cómo *estás*?',
+        sentDate: new Date().toISOString(),
+        // randomize if is th
+        read: Math.random() > 0.5,
+      },
+    })).map((chat, index) => {
+      return {
+        ...chat,
+        user: {
+          ...chat.user,
+          photoUrl: `https://randomuser.me/api/portraits/thumb/men/${
+            index + 1
+          }.jpg`,
+        },
+      };
+    });
+  };
+
   return (
-    <PaperProvider>
-      <Text>GeoFencesChatsScreen</Text>
-      <ChatsList {...props} />
-    </PaperProvider>
+    <Layout>
+      {!isHeaderHidden && (
+        <Animated.View
+          style={[
+            {},
+            {
+              transform: [{translateY: translateYAnim}],
+            },
+          ]}>
+          <View style={styles.contactsShortcutContainer}>
+            <ContactsShortcut
+              contacts={fakeChats(10).map(mock => ({
+                imgUser: mock.user.photoUrl,
+                lastName: mock.user.name,
+                name: mock.user.name,
+                isOnline: Math.random() > 0.5,
+              }))}
+            />
+          </View>
+        </Animated.View>
+      )}
+      <Animated.View
+        style={[
+          {flex: 1},
+          {
+            transform: [{translateY: translateYAnim}],
+          },
+        ]}>
+        <View style={styles.searchContainer}>
+          <NbSearchBar />
+        </View>
+        <ScrollView
+          style={styles.chatsContainer}
+          onScroll={onScroll}
+          scrollEventThrottle={16}>
+          <ChatsList {...props} chats={fakeChats(80)} />
+        </ScrollView>
+      </Animated.View>
+    </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contactsShortcutContainer: {
+    padding: 16,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchContainer: {paddingLeft: 16, paddingRight: 16},
+  chatsContainer: {
+    marginTop: 16,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+});
