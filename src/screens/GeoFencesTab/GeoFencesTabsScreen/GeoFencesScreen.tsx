7723b078450322631e93f30a2cd34a70f1053414
José María CL <chema9703@gmail.com>
2023-10-01 13:22:07 -0600

    feat: first approach to draw geofence

--- src/screens/GeoFencesTab/GeoFencesTabsScreen/GeoFencesScreen.tsx
+++ src/screens/GeoFencesTab/GeoFencesTabsScreen/GeoFencesScreen.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {PaperProvider} from 'react-native-paper';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import {StyleSheet, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Polygon} from 'react-native-maps';
+import {Alert, StyleSheet, View} from 'react-native';
 
 export const GeoFencesScreen = (): JSX.Element => {
+  const [state] = React.useState({
+    coordinates: [
+      {latitude: 18.849195, longitude: -97.104676},
+      {latitude: 18.848809, longitude: -97.104461},
+      {latitude: 18.848444, longitude: -97.105405},
+      {latitude: 18.849061, longitude: -97.105514},
+    ],
+  });
+
   return (
     <PaperProvider>
       <View style={styles.container}>
@@ -11,12 +20,21 @@ export const GeoFencesScreen = (): JSX.Element => {
           provider={PROVIDER_GOOGLE}
           style={styles.map}
           initialRegion={{
-            latitude: 18.859091,
-            longitude: -97.0967663,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
+            latitude: 18.849521,
+            longitude: -97.105003,
+            latitudeDelta: 0.0022,
+            longitudeDelta: 0.0021,
+          }}>
+          <Polygon
+            tappable
+            onPress={() => Alert.alert('Polygon pressed')}
+            key={'asd'}
+            coordinates={state.coordinates}
+            strokeColor="#F00"
+            fillColor="rgba(255,0,0,0.5)"
+            strokeWidth={1}
+          />
+        </MapView>
       </View>
     </PaperProvider>
   );
