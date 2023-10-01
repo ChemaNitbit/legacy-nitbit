a619c3c90cf807dc08af1e554bc1c3b9ac854c67
José María CL <chema9703@gmail.com>
2023-10-01 00:57:00 -0600

    feat: show a google map in the geofences screen

--- src/screens/GeoFencesTab/GeoFencesTabsScreen/GeoFencesScreen.tsx
+++ src/screens/GeoFencesTab/GeoFencesTabsScreen/GeoFencesScreen.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import {PaperProvider, Text} from 'react-native-paper';
+import {PaperProvider} from 'react-native-paper';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
 
 export const GeoFencesScreen = (): JSX.Element => {
   return (
     <PaperProvider>
-      <Text>GeoFencesScreen</Text>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: 18.859091,
+            longitude: -97.0967663,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
     </PaperProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
